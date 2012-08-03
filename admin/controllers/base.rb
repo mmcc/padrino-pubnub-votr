614f2498b9f73cc57485591e1944e9a9fee2f15c
Matt McClure <matt@htbx.net>
2012-08-03 01:56:19 -0400

    starting admin-facelift

--- admin/controllers/base.rb
+++ admin/controllers/base.rb
@@ -1,6 +1,8 @@
 Admin.controllers :base do
 
   get :index, :map => "/" do
+  	@current_round = Round.current.first
+  	@upcoming_rounds = Round.where("start_time > ?", DateTime.now)
     render "base/index"
   end
 end
