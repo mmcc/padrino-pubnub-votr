a8741e785ac99b9f6e0748d337ab59069a589c83
Matt McClure <matt@htbx.net>
2012-08-03 02:16:19 -0400

    landing page for admin panel started

--- admin/controllers/base.rb
+++ admin/controllers/base.rb
@@ -1,8 +1,9 @@
 Admin.controllers :base do
 
   get :index, :map => "/" do
-  	@current_round = Round.current.first
-  	@upcoming_rounds = Round.where("start_time > ?", DateTime.now)
+  	@rounds = Round.scoped.includes(:songs).where("end_time > ?", DateTime.now)
+  	@current_round = @rounds.current.first
+  	@upcoming_rounds = @rounds.where("start_time > ?", DateTime.now)
     render "base/index"
   end
 end
